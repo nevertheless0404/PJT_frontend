e563d0b95201e1257cfb3096f8c2b9b2f57197bd
sebin kwon <sbkwon98@gmail.com>
2022-11-30 00:26:00 +0900

    fix : npm run serve 오류 수정 [권세빈]

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <b-button variant="primary">Button</b-button>
     </nav>
     <router-view/>
   </div>
