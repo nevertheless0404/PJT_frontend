bdda0077e954a335852d3323284cd0ea1f05aeec
sebin kwon <sbkwon98@gmail.com>
2022-11-30 09:50:31 +0900

    feat : signup 페이지 추가 [권세빈]

--- src/router/index.js
+++ src/router/index.js
@@ -11,12 +11,19 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/login',
+    name: 'login',
     // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
+    // this generates a separate chunk (login.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () =>
+      import(/* webpackChunkName: "login" */ '../views/LoginView.vue')
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: () =>
+      import(/* webpackChunkName: "signup" */ '../views/SignupView.vue')
   }
 ]
 
