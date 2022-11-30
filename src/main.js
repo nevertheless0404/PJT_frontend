b9608070eef1808e258702362cb4afce016840bd
김준환 <hvvany@gimjunhwan-ui-MacBookAir.local>
2022-11-30 09:42:59 +0900

    feat:axios json 요청 받기 테스트

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+// axios를 import
 
 Vue.config.productionTip = false
 
@@ -10,3 +12,7 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+Vue.prototype.$eventBus = new Vue()
+Vue.prototype.$axios = axios
+// 다른 컴포넌트에서는 import 없이 this.$axios로 사용가능
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button v-on:click="getMultiData">get data</button>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      allPeopleList: []
+    }
+  },
+  created () {
+    this.getMultiData()
+  },
+  methods: {
+    async getMultiData () {
+      try {
+        this.allPeopleList = await axios
+          .get('https://jsonplaceholder.typicode.com/users/')
+          .then((res) => {
+            console.log(res.staus)
+            console.log(res.data)
+          })
+          .catch((error) => {
+            console.log(error)
+          })
+          .finally(() => {
+            console.log('항상 마지막에 실행')
+          })
+        console.log(this.allPeopleList)
+      } catch (error) {
+        console.error(error)
+      }
+    }
   }
 }
 </script>
