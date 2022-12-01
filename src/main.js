9f866397aed714b05c21b06784cf41df5e04fa0a
sebin kwon <sbkwon98@gmail.com>
2022-12-01 17:54:51 +0900

    feat : bootstrap 추가

--- src/main.js
+++ src/main.js
@@ -4,7 +4,16 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 // axios를 import
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
 Vue.config.productionTip = false
 
 new Vue({
