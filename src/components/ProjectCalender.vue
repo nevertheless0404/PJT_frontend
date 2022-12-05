f808b09f4d044abf73fb7d4821a4be134d6c4643
sebin kwon <sbkwon98@gmail.com>
2022-12-05 14:03:32 +0900

    feat/design : project/index 달력 추가, project/create/member 버튼 디자인

--- src/components/ProjectCalender.vue
+++ src/components/ProjectCalender.vue
@@ -1,101 +1,22 @@
+<script setup>
+import { reactive } from 'vue'
+import '@fullcalendar/core/vdom' // solves problem with Vite
+import FullCalendar from '@fullcalendar/vue'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import listPlugin from '@fullcalendar/list'
+
+const options = reactive({
+  plugins: [dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin],
+  initialView: 'dayGridMonth'
+})
+</script>
+
 <template>
-  <div class="wrapper">
-    <!-- <header>
-      <p class="current-date">November 2022</p>
-      <div class="icons">
-        <i class="bi bi-chevron-left"></i>
-        <i class="bi bi-chevron-right"></i>
-      </div>
-    </header>
-    <div class="calendar">
-      <ul class="weeks">
-        <li>Sun</li>
-        <li>Mon</li>
-        <li>Tue</li>
-        <li>Wed</li>
-        <li>Thu</li>
-        <li>Fri</li>
-        <li>Sat</li>
-      </ul>
-      <ul class="days">
-        <li>30</li>
-        <li>31</li>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-        <li>5</li>
-        <li>6</li>
-        <li>7</li>
-        <li>8</li>
-        <li>9</li>
-        <li>10</li>
-        <li>11</li>
-        <li>12</li>
-        <li>13</li>
-        <li>14</li>
-        <li>15</li>
-        <li>16</li>
-        <li>17</li>
-        <li>18</li>
-        <li>19</li>
-        <li>20</li>
-        <li>21</li>
-        <li>22</li>
-        <li>23</li>
-        <li>24</li>
-        <li>25</li>
-        <li>26</li>
-        <li>27</li>
-        <li>28</li>
-        <li>29</li>
-        <li>30</li>
-      </ul>
-    </div> -->
+  <div>
+    <FullCalendar v-bind:options="options" />
   </div>
 </template>
 
-<script>
-export default {
-  components: {},
-  data() {
-    return {
-      sampleData: ''
-    }
-  },
-  setup() {},
-  created() {},
-  mounted() {},
-  unmounted() {},
-  methods: {}
-}
-</script>
-
-<style scoped>
-/* body{
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  min-height: 100vh;
-  background: gray;
-} */
-/* .wrapper{
-  width: 450px;
-  background: rgb(255, 209, 209);
-}
-.wrapper header{
-  display: flex;
-  align-items: center;
-  padding: 25px 30px 10px;
-  justify-content: space-between;
-}
-header .current-date{
-  font-size: 1.45rem;
-  font-weight: 500;
-}
-.calendar ul{
-  display: flex;
-  list-style: none;
-  flex-wrap: wrap;
-} */
-</style>
+<style scoped></style>
