40e50d61ab79b9b3fc98f2b8cd6fc481e32f6d07
sebin kwon <sbkwon98@gmail.com>
2022-12-06 17:49:21 +0900

    feat : 라우트 재설정 및 프로젝트/멤버 생성 연결

--- src/views/MemberCreateView.vue
+++ src/views/MemberCreateView.vue
@@ -1,54 +1,89 @@
 <template>
-  <div class="container">
-    <div>
-      <div class="d-flex justify-content-between">
-        <label for="exampleInputEmail1" class="form-label">프로젝트 팀원</label>
-        <div class="d-flex mb-2">
-          <div id="emailHelp" class="form-text me-2">
-            <b-button size="sm" variant="outline-primary" @click="addMember"
-              ><i class="bi bi-plus-lg"></i> 추가</b-button
+  <div>
+    <ProjectIndexNav />
+    <!-- {{ this.members }} -->
+    <div class="container mt-4">
+      <form @submit.prevent="submitMember">
+        <div>
+          <div class="d-flex justify-content-between">
+            <label for="exampleInputEmail1" class="form-label"
+              >프로젝트 팀원</label
             >
+            <div class="d-flex mb-2">
+              <div id="emailHelp" class="form-text me-2">
+                <b-button size="sm" variant="outline-primary" @click="addMember"
+                  ><i class="bi bi-plus-lg"></i> 추가</b-button
+                >
+              </div>
+              <div id="emailHelp" class="form-text">
+                <b-button
+                  size="sm"
+                  variant="outline-danger"
+                  @click="removeMember"
+                  ><i class="bi bi-dash-lg"></i> 삭제</b-button
+                >
+              </div>
+            </div>
           </div>
-          <div id="emailHelp" class="form-text">
-            <b-button size="sm" variant="outline-danger" @click="removeMember"
-              ><i class="bi bi-dash-lg"></i> 삭제</b-button
-            >
-          </div>
+          <input
+            type="email"
+            class="form-control mb-3"
+            id="exampleInputEmail1"
+            placeholder="Email"
+            aria-describedby="emailHelp"
+            v-model="member.user"
+            :key="id"
+            v-for="(member, id) in members"
+          />
         </div>
-      </div>
-      <input
-        type="email"
-        class="form-control mb-3"
-        id="exampleInputEmail1"
-        placeholder="Email"
-        aria-describedby="emailHelp"
-        v-model="member.user"
-        :key="id"
-        v-for="(member, id) in members"
-      />
+        <button type="submit" class="btn btn-primary">다음</button>
+      </form>
+      <!-- <div v-html="memberHtml" v-if="isCreate" class="mb-3"></div> -->
     </div>
-    <!-- <div v-html="memberHtml" v-if="isCreate" class="mb-3"></div> -->
   </div>
 </template>
 
 <script>
+import ProjectIndexNav from '@/components/ProjectIndexNav.vue'
+import { memberCreate, projectPk } from '@/api/index'
 export default {
-  components: {},
+  components: { ProjectIndexNav },
   data() {
     return {
       members: [
         {
-          project: '',
+          id: '',
           user: ''
         }
       ]
     }
   },
   setup() {},
-  created() {},
+  created() {
+    projectPk() // 위에서 임포트한 통신 메소드이다. 렌더링시 생성(created)되도록 만든다.
+      .then((response) => {
+        console.log(response)
+        this.members = response.data
+      }) // 성공하면 json 객체를 받아온다.
+      .catch((error) => console.log(error))
+  },
   mounted() {},
   unmounted() {},
   methods: {
+    async submitMember() {
+      // API 요청시 전달할 userData 객체
+      let stringMember = ''
+      this.members.forEach((ele) => {
+        stringMember += ele.name + ' '
+      })
+      const memberData = {
+        project: this.project,
+        user: stringMember
+      }
+
+      await memberCreate(memberData)
+      this.$router.push('/project/')
+    },
     addMember() {
       this.members.push({
         user: ''
--- src/views/ProjectIndexView.vue
+++ src/views/ProjectIndexView.vue
@@ -1,31 +1,70 @@
 <template>
   <div>
-    <router-link :to="{ name: 'projectcreate' }" class="btn btn-outline-primary"
-      >프로젝트 생성</router-link
-    >
-    <ProjectCalender />
-    <router-view></router-view>
+    <ProjectIndexNav />
+    <div class="container d-flex justify-content-evenly mt-4">
+      <ProjectCalender />
+      <!-- <p>{{ this.projects }}</p> -->
+      <div>
+        <div v-for="(project, id) in projects" :key="id">
+          <div class="pjts mb-3">
+            <span class="pjtTitle">{{ project.title }}</span>
+          </div>
+        </div>
+        <router-link
+          :to="{ name: 'projectcreate', params: {} }"
+          class="btn btn-outline-primary"
+          >프로젝트 생성</router-link
+        >
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import ProjectIndexNav from '@/components/ProjectIndexNav.vue'
 import ProjectCalender from '@/components/ProjectCalender.vue'
-
+import { projectIndex } from '@/api/index'
 export default {
   components: {
-    ProjectCalender
+    ProjectCalender, ProjectIndexNav
   },
   data() {
     return {
-      sampleData: ''
+      projects: [{ id: '' }, { title: '' }, { start_at: '' }, { end_at: '' }]
     }
   },
   setup() {},
-  created() {},
+  created() {
+    projectIndex() // 위에서 임포트한 통신 메소드이다. 렌더링시 생성(created)되도록 만든다.
+      .then((response) => {
+        console.log(response)
+        this.projects = response.data
+        //   this.projects.push({
+        //     deadline: ''
+        //   })
+        //   this.projects.forEach((ele) => {
+        //   stringSkill += ele.name + ' '
+        // })
+        //   const year = Number(this.projects.end_at.slice(0, 4))
+        //   console.log(year)
+      }) // 성공하면 json 객체를 받아온다.
+      .catch((error) => console.log(error))
+  },
   mounted() {},
   unmounted() {},
   methods: {}
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.pjts {
+  background-color: white;
+  box-shadow: 5px 9px 16px 0px #0d224216;
+  width: 300px;
+  padding: 20px;
+  border-radius: 10px;
+}
+.pjtTitle {
+  font-weight: 500;
+}
+</style>
