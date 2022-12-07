637d8e98297d85c70fe2dffc4ed228d7719a651f
sebin kwon <sbkwon98@gmail.com>
2022-12-07 16:05:24 +0900

    feat : nav/kanban board develop

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,45 +1,82 @@
 <template>
-  <nav class="navbar navbar-expand-lg bg-light sticky-top">
-    <div class="container-fluid px-5">
-      <routerLink class="navbar-brand" :to="{ name: 'projectindex' }">
-        <img src="@/assets/images/logo.png" width="120px" />
-      </routerLink>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div
-        class="collapse navbar-collapse d-flex justify-content-end"
-        id="navbarNav"
-      >
-        <ul class="navbar-nav" v-if="!user">
-          <li class="nav-item">
-            <routerLink class="nav-link" to="/login">로그인</routerLink>
-          </li>
-          <li class="nav-item">
-            <routerLink class="nav-link" to="/signup">회원가입</routerLink>
-          </li>
-        </ul>
-        <ul class="navbar-nav" v-if="user">
-          <li class="nav-item">
-            <a class="nav-link" href="javascript:void(0)" @click="handleClick"
-              >로그아웃</a
-            >
-          </li>
-          <li class="nav-item">
-            <routerLink class="nav-link" to="/project">프로젝트</routerLink>
-          </li>
-        </ul>
+  <div>
+    <nav class="navbar navbar-expand-lg bg-light sticky-top">
+      <div class="container-fluid px-5">
+        <routerLink class="navbar-brand" :to="{ name: 'projectindex' }">
+          <img src="@/assets/images/logo.png" width="120px" />
+        </routerLink>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div
+          class="collapse navbar-collapse d-flex justify-content-end"
+          id="navbarNav"
+        >
+          <ul class="navbar-nav" v-if="!user">
+            <li class="nav-item">
+              <routerLink class="nav-link" to="/login">로그인</routerLink>
+            </li>
+            <li class="nav-item">
+              <routerLink class="nav-link" to="/signup">회원가입</routerLink>
+            </li>
+          </ul>
+          <ul class="navbar-nav" v-if="user">
+            <li class="nav-item">
+              <a class="nav-link" href="javascript:void(0)" @click="handleClick"
+                >로그아웃</a
+              >
+            </li>
+            <li class="nav-item">
+              <routerLink class="nav-link" to="/project">프로젝트</routerLink>
+            </li>
+          </ul>
+        </div>
       </div>
+    </nav>
+    <div>
+      <b-navbar toggleable="lg" type="dark" variant="info">
+        <b-navbar-brand :to="{ name: 'projectindex' }"
+          ><img src="@/assets/images/logo.png" width="120px"
+        /></b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item href="#">Link</b-nav-item>
+            <b-nav-item href="#" disabled>Disabled</b-nav-item>
+          </b-navbar-nav>
+
+          <!-- Right aligned nav items -->
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item-dropdown text="{{}}" right>
+              <b-dropdown-item href="#">EN</b-dropdown-item>
+              <b-dropdown-item href="#">ES</b-dropdown-item>
+              <b-dropdown-item href="#">RU</b-dropdown-item>
+              <b-dropdown-item href="#">FA</b-dropdown-item>
+            </b-nav-item-dropdown>
+
+            <b-nav-item-dropdown right>
+              <!-- Using 'button-content' slot -->
+              <template #button-content>
+                <em>User</em>
+              </template>
+              <b-dropdown-item href="#">Profile</b-dropdown-item>
+              <b-dropdown-item href="#">Logout</b-dropdown-item>
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
     </div>
-  </nav>
+  </div>
 </template>
 
 <script>
