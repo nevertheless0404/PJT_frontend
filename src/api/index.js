cdfd9c17ddccea05d944d26ab2d7e6e48f9b6ac3
kimdakyeom <74820869+kimdakyeom@users.noreply.github.com>
2022-12-09 14:36:29 +0900

    fix: 알림 읽음 처리

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,6 @@ import axios from 'axios'
 function registerUser(userData) {
   // 요청할 URL
   const url = 'http://127.0.0.1:8000/api/accounts/v1/registration/'
-  console.log('userData :', userData)
   return axios.post(url, userData, {
     'Content-Type': 'application/json'
   })
@@ -12,7 +11,6 @@ function registerUser(userData) {
 function projectIndex() {
   // 요청할 URL
   const url = 'http://127.0.0.1:8000/project/'
-  // console.log('projectData :', projectData)
   return axios.get(url, {
     headers: {
       'Content-Type': 'application/json',
@@ -24,7 +22,6 @@ function projectIndex() {
 function projectCreate(projectData) {
   // 요청할 URL
   const url = 'http://127.0.0.1:8000/project/'
-  console.log('projectData :', projectData)
   return axios.post(url, projectData, {
     headers: {
       'Content-Type': 'application/json',
@@ -36,7 +33,6 @@ function projectCreate(projectData) {
 function projectGet(project) {
   // 요청할 URL
   const url = `http://127.0.0.1:8000/project/${project}/`
-  console.log(project)
   return axios.get(url, {
     headers: {
       'Content-Type': 'application/json',
@@ -48,7 +44,6 @@ function projectGet(project) {
 function projectUpdate(project, projectData) {
   // 요청할 URL
   const url = `http://127.0.0.1:8000/project/${project}/`
-  console.log('projectData :', projectData)
   return axios.put(url, projectData, {
     headers: {
       'Content-Type': 'application/json',
@@ -60,7 +55,6 @@ function projectUpdate(project, projectData) {
 function markdownGet(project) {
   // 요청할 URL
   const url = `http://127.0.0.1:8000/${project}/markdown`
-  console.log(project)
   return axios.get(url, {
     headers: {
       'Content-Type': 'application/json',
@@ -85,7 +79,6 @@ function memberCreate(memberData) {
   // 요청할 URL
   const url = `http://127.0.0.1:8000/memberadmin/${memberData.id}/`
   console.log('memberData :', memberData)
-  console.log(url)
   return axios.post(url, memberData, {
     headers: {
       'Content-Type': 'application/json',
@@ -93,5 +86,28 @@ function memberCreate(memberData) {
     }
   })
 }
+
+function NotificationGet() {
+  // 요청할 URL
+  const url = `http://127.0.0.1:8000/notification`
+  return axios.get(url, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + localStorage.getItem('access_token')
+    }
+  })
+}
+
+function isRead(notificationPk) {
+  // 요청할 URL
+  const url = `http://127.0.0.1:8000/isread/${notificationPk}`
+  return axios.put(url, notificationPk, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + localStorage.getItem('access_token')
+    }
+  })
+}
+
 // 함수 export
-export { registerUser, projectIndex, projectCreate, memberCreate, projectGet, projectUpdate, markdownGet }
+export { registerUser, projectIndex, projectCreate, memberCreate, projectGet, projectUpdate, markdownGet, NotificationGet, isRead }
