d6fcb696180cbc683023a5a4150163db954221b5
kimdakyeom <74820869+kimdakyeom@users.noreply.github.com>
2022-12-11 01:13:03 +0900

    fix: 공지사항 수정

--- src/api/index.js
+++ src/api/index.js
@@ -200,10 +200,11 @@ function informList(pjt_pk) {
   })
 }
 
-function InformUpdate(project, projectData) {
+function InformPut(project, informData) {
+  console.log(informData)
   // 요청할 URL
-  const url = `http://127.0.0.1:8000/project/${project}/`
-  return axios.put(url, projectData, {
+  const url = `http://127.0.0.1:8000/informs/${project}/detail`
+  return axios.put(url, informData, {
     headers: {
       'Content-Type': 'application/json',
       Authorization: 'Bearer ' + localStorage.getItem('access_token')
@@ -229,5 +230,5 @@ export {
   isRead,
   informCreate,
   informList,
-  InformUpdate
+  InformPut
 }
