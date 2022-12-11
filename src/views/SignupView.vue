5bab5859db904bd7fdc464f297b7d8ac439dc6d6
김준환 <hvvany@gimjunhwan-ui-MacBookAir.local>
2022-12-12 02:32:03 +0900

    feat:회원가입 오류 메시지 구현

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -39,6 +39,12 @@
       <button type="submit" class="btn w-75 my-3 btn-signup">회원가입</button>
       <!-- 결과 메시지 출력 -->
       <p>{{ logMessage }}</p>
+      <p v-if="errors.length > 0">
+        Please correct the following error(s):
+        <ul>
+          <li :key="idx" v-for="(error, idx) in errors">{{ error }}</li>
+        </ul>
+      </p>
     </form>
   </div>
 </template>
@@ -54,29 +60,52 @@ export default {
       password1: '',
       password2: '',
       // log
-      logMessage: ''
+      logMessage: '',
+      errors:[]
     }
   },
   methods: {
     async submitForm() {
+      this.checkForm()
       // API 요청시 전달할 userData 객체
-      const userData = {
-        email: this.email,
-        password1: this.password1,
-        password2: this.password2
+      if(this.errors.length < 1){
+        console.log('!!!!!!!!')
+        const userData = {
+          email: this.email,
+          password1: this.password1,
+          password2: this.password2
+        }
+        const { data } = await registerUser(userData)
+        this.logMessage = `${data.email} 님이 가입되었습니다.`
+
+        // 가입 후 폼 초기화
+        this.initForm()
+        this.$router.push('/login')
       }
-      const { data } = await registerUser(userData)
-
-      this.logMessage = `${data.email} 님이 가입되었습니다.`
-
-      // 가입 후 폼 초기화
-      this.initForm()
-      this.$router.push('/login')
     },
     initForm() {
       this.email = ''
       this.password1 = ''
       this.password2 = ''
+    },
+    checkForm() {
+      console.log('체크폼 실행')
+      this.errors = []
+      if(this.email === ''){
+        this.errors.push("Email required")
+      }
+      if(this.password1 === '') {
+        this.errors.push("Password required")
+      }
+      if(this.email.split('@')[0].length < 4) {
+        this.errors.push("Too short you email length. Please length +3 before '@'")
+      }
+      if(this.password1.length < 9) {
+        this.errors.push("Too short you password length. Please password +8")
+      }
+      if(this.password1 != this.password2) {
+        this.errors.push("Password does not matching")
+      }
     }
   }
 }
