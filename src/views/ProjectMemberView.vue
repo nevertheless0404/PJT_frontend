a97568b87ccf630fb976a835c43e476bf3347f0b
nevertheless0404 <nevertheless.hi@gmail.com>
2022-12-13 02:05:30 +0900

    style : 멤버 추가 스타일

--- src/views/ProjectMemberView.vue
+++ src/views/ProjectMemberView.vue
@@ -7,7 +7,7 @@
         <form @submit.prevent="postmember" class='form1 mb-5 input-wrap'>
           <div class="w-100 d-flex">
             <input @keyup="searchEmail" type="email" class="search_input" placeholder="Email" v-model="memberInput" autocomplete="off">
-            <button type="submit" class="search_btn btn btn-primary">추가</button>
+            <button type="submit" class="search_btn">추가</button>
           </div>
           <div id="suggestions_pannel" v-for="(user, idx) in userList" :key="idx">
             <p @click="updateInput(user.email)">{{user.email}}</p>
@@ -16,15 +16,17 @@
         <div class="allmember">
           <div class="memberbox" :key="id" v-for="(member, id) in members">
             <span class="pjtmember">{{ member.user }}</span>
-            <span v-if="(member.leader===true)">팀장</span>
-            <span v-if="(member.leader===false)">팀원
-              <button @click="changeleader" :data-id="member.id" v-if="(user.email===teamLeader)" class="btn btn-dark">
-                팀장 위임
-              </button>
-              <button @click="deletemember" :data-id="member.id" v-if="(user.email===teamLeader)" class="btn btn-danger">
-                팀원 삭제
-              </button>
-            </span>
+            <div class="member">
+              <span v-if="(member.leader===true)">팀장</span>
+              <span v-if="(member.leader===false)">팀원
+                <button @click="deletemember" :data-id="member.id" v-if="(user.email===teamLeader)" class="btn2 float-end">
+                  팀원 삭제
+                </button>
+                <button @click="changeleader" :data-id="member.id" v-if="(user.email===teamLeader)" class="btn3 float-end">
+                  팀장 위임
+                </button>
+              </span>
+            </div>
           </div>
         </div>
       </div>
@@ -142,7 +144,8 @@ export default {
   padding: 20px;
   border-radius: 10px;
   margin-bottom: 30px;
-  border: #D9D9D9 solid 2px;
+  border: #D9D9D9 solid 1px;
+  overflow: auto;
 }
 
 .pjtmember {
@@ -150,6 +153,29 @@ export default {
   text-decoration: none;
 }
 
+.member{
+  margin-top: 2px;
+}
+
+.search_btn{
+  margin-left: 7px;
+  color: white;
+  background-color: #3485FF;
+  width: 75px;
+  height: 40px;
+  border-radius: 10px;
+  border: #D9D9D9 solid 0px;
+  text-decoration: none;
+  text-align : center;
+  box-shadow: inset 0px 0px 0px #FFC062;
+  display: block;
+  -webkit-transition: all 0.8s cubic-bezier(.5, .24, 0, 1);
+  transition: all 0.8s cubic-bezier(.5, .24, 0, 1)
+}
+
+.seach_btn{
+  box-shadow: inset 300px 0px 0px 0px #FFC062;
+}
 .btn1 {
   margin: auto;
   color: white;
@@ -160,8 +186,6 @@ export default {
   border: #D9D9D9 solid 0px;
   text-decoration: none;
   text-align : center;
-
-
   box-shadow: inset 0px 0px 0px #FFC062;
   display: block;
   -webkit-transition: all 0.8s cubic-bezier(.5, .24, 0, 1);
@@ -169,10 +193,48 @@ export default {
 }
 
 .btn1:hover {
-
   box-shadow: inset 300px 0px 0px 0px #FFC062;
 }
 
+.btn2 {
+  margin-left: 1px;
+  color: white;
+  background-color: #990101;
+  font-size: 12px;
+  height: 30px;
+  border-radius: 10px;
+  border: #D9D9D9 solid 1px;
+  text-decoration: none;
+  text-align : center;
+  box-shadow: inset 0px 0px 0px #FFC062;
+  display: block;
+  -webkit-transition: all 0.8s cubic-bezier(.5, .24, 0, 1);
+  transition: all 0.8s cubic-bezier(.5, .24, 0, 1)
+}
+
+.btn2:hover {
+  box-shadow: inset 300px 0px 0px 0px #FFC062;
+}
+
+.btn3 {
+  margin: auto;
+  color: white;
+  background-color: #434343;
+  font-size: 12px;
+  height: 30px;
+  border-radius: 10px;
+  border: #D9D9D9 solid 0px;
+  text-decoration: none;
+  text-align : center;
+  box-shadow: inset 0px 0px 0px #FFC062;
+  display: block;
+  -webkit-transition: all 0.8s cubic-bezier(.5, .24, 0, 1);
+  transition: all 0.8s cubic-bezier(.5, .24, 0, 1)
+}
+
+.btn3:hover {
+  box-shadow: inset 300px 0px 0px 0px #FFC062;
+}
 .allmember{
   display: flex;
   align-items: center;
@@ -187,6 +249,7 @@ export default {
     color: #777;
     line-height: 40px;
     border: none;
+    border-bottom: 1px solid #bcbcbc;
     background: none;
 }
 
